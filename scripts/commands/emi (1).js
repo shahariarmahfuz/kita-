da222c63d0143d1389d6b91c396e01bbb6820eb7
shahariarmahfuz <162718500+shahariarmahfuz@users.noreply.github.com>
2024-03-26 16:47:07 +0600

    Add files via upload

---
+++ scripts/commands/emi (1).js
@@ -0,0 +1,37 @@
+
+let url = "https://ai-tools.replit.app";
+const { get } = require('axios'), fs = require('fs');
+let f = __dirname+'/cache/emi.png';
+
+module.exports = {
+  config: {
+    name: "emi",
+  	version: "1.0.0",
+  	permssion: 0,
+    credits: "Deku",
+    prefix: true,
+	  description: "Generate image in emi",
+  	category: "AI",
+  	usages: "[prompt]",
+  	cooldowns: 5,
+  },
+  run: async function({api, event, args}){
+    function r(msg){
+      api.sendMessage(msg, event.threadID, event.messageID);
+    }
+    
+    if (!args[0]) return r('Missing prompt!');
+    
+    const a = args.join(" ")
+    if (!a) return r('Missing prompt!');
+    try {
+    const d = (await get(url+'/emi?prompt='+a, {
+      responseType: 'arraybuffer'
+    })).data;
+    fs.writeFileSync(f, Buffer.from(d, "utf8"));
+    return r({attachment: fs.createReadStream(f, () => fs.unlinkSync(f))});
+    } catch (e){
+      return r(e.message)
+    }
+  }
+}
---
+++ scripts/commands/gemini (2).js
@@ -0,0 +1,37 @@
+module.exports.config = {
+  name: "gemi",
+  version: "1.0.0",
+  permssion: 0,
+  credits: "Deku",
+  prefix: true,
+  description: "this command will help you to answer your questions!",
+  category: "Artificial Intelligence",
+  usages: "(name_of_cmd) [query]",
+  cooldowns: 0
+};  
+  
+module.exports.run = async ({ api, event, args }) => {
+    const axios = require("axios");
+    let prompt = args.join(" "),
+      uid = event.senderID,
+      url;
+    if (!prompt) return api.sendMessage(`Please enter a prompt.`, event.threadID, event.messageID);
+    
+    try {
+      const apis = `https://gemini-api.replit.app`;
+      if (event.type == "message_reply"){
+        if (event.messageReply.attachments[0]?.type == "photo"){
+        url = encodeURIComponent(event.messageReply.attachments[0].url);
+        const res = (await axios.get(apis + "/gemini?prompt="+prompt+"&url="+url+"&uid="+uid)).data;
+        return api.sendMessage(`${res.gemini}`, event.threadID, event.messageID)
+        } else {
+          return api.sendMessage('Please reply to an image.', event.threadID, event.messageID)
+        }
+      }
+      const rest = (await axios.get(apis + "/gemini?prompt=" + prompt + "&uid=" + uid)).data;
+      return api.sendMessage(rest.gemini, event.threadID, event.messageID)
+    } catch (e) {
+      console.log(e);
+      return api.sendMessage(e.message, event.threadID, event.messageID);
+    } //end of catch
+  };
